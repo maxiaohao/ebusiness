1e6914d63d49599f39f2c474b96d23740dd84963
chensy <18915982365@189.cn>
2016-11-25 10:12:42 +0800

    开源电商平台初始工程

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemService.java
@@ -0,0 +1,12 @@
+package com.taotao.service;
+import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemDesc;
+
+public interface ItemService{
+	public TbItem getItemById(Long itemId);
+	public EasyUIDataGridResult getItemList(int page,int rows);
+	TaotaoResult createItem(TbItem item, String itemDesc);
+	public String getItemParamHtml(Long itemId);
+}
