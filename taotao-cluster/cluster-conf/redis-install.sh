793490addb80262cf5c69cd65f97c8e152f107c1
maxiaohao <maxiaohao@gmail.com>
2016-12-03 11:15:28 +0800

    Fix: redis nodes all listen on 0.0.0.0:6379 rather than 127.0.0.1:6379

--- taotao-cluster/cluster-conf/redis-install.sh
+++ taotao-cluster/cluster-conf/redis-install.sh
@@ -16,6 +16,7 @@ cp ./src/redis-trib.rb /opt/redis
 
 sed -i 's/# cluster-enabled yes/cluster-enabled yes/g' /opt/redis/redis.conf
 sed -i 's/daemonize no/daemonize yes/g' /opt/redis/redis.conf
+sed -i 's/bind 127\.0\.0\.1/bind 0\.0\.0\.0/g' /opt/redis/redis.conf
 
 gem install $INST_STUFF/redis-3.2.2.gem
 
