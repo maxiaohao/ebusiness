e279c1d45812302477f774464d10dd5637f923d6
DongDebao <dongdebao@gmail.com>
2016-12-19 20:11:15 +0800

    修改搜索下一页

--- taotao-portal/src/main/webapp/js/base-2011.js
+++ taotao-portal/src/main/webapp/js/base-2011.js
@@ -27,11 +27,11 @@ function readCookie(a) {
     return null
 }
 function addToFavorite() {
-    var a = "http://www.jd.com/", b = "\u4eac\u4e1cJD.COM-\u7f51\u8d2d\u4e0a\u4eac\u4e1c\uff0c\u7701\u94b1\u53c8\u653e\u5fc3";
+    var a = "", b = "\u4eac\u4e1cJD.COM-\u7f51\u8d2d\u4e0a\u4eac\u4e1c\uff0c\u7701\u94b1\u53c8\u653e\u5fc3";
     document.all ? window.external.AddFavorite(a, b) : window.sidebar && window.sidebar.addPanel ? window.sidebar.addPanel(b, a, "") : alert("\u5bf9\u4e0d\u8d77\uff0c\u60a8\u7684\u6d4f\u89c8\u5668\u4e0d\u652f\u6301\u6b64\u64cd\u4f5c!\n\u8bf7\u60a8\u4f7f\u7528\u83dc\u5355\u680f\u6216Ctrl+D\u6536\u85cf\u672c\u7ad9\u3002"), createCookie("_fv", "1", 30, "/;domain=jd.com")
 }
 function search(a) {
-    var b = "http://search.jd.com/Search?keyword={keyword}&enc={enc}{additional}", c = search.additinal || "", d = document.getElementById(a), e = d.value;
+    var b = "http://search.taotao.com/Search?keyword={keyword}&enc={enc}{additional}", c = search.additinal || "", d = document.getElementById(a), e = d.value;
     if (e = e.replace(/^\s*(.*?)\s*$/, "$1"), e.length > 100 && (e = e.substring(0, 100)), "" == e)
         return window.location.href = window.location.href, void 0;
     var f = 0;
